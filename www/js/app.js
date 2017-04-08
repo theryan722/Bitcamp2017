460ee3494e77abe9136bd4dc440a4785cd0612ed
Ryan O'Day <romanguy722@gmail.com>
2017-04-08 04:39:02 -0400

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -10,3 +10,14 @@ function onDeviceReady() {
 function onBackKeyDown() {
     mainView.router.back();
 }
+
+//Load the contents of the drawer
+loadElementHtml('#drawer', 'draweritems.html', undefined);
+
+//Load the contents of the url into the specified id
+function loadElementHtml(element, url, callback = undefined) {
+    $$.get(url, undefined, function (data) {
+        $$(element).html(data);
+        if (typeof callback !== 'undefined') { callback(); }
+    });
+}
