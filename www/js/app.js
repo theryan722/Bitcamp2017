4b4987023e4e974b497b1da60515914d6d023ff8
Ryan O'Day <romanguy722@gmail.com>
2017-04-08 04:14:53 -0400

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,12 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('backbutton', onBackKeyDown, false);
 
+//Device is ready
+function onDeviceReady() {
+    //
+}
+
+//Handle android back key
+function onBackKeyDown() {
+    mainView.router.back();
+}
