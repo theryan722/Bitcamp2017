b3a4ae9a58710bc15f9ac2055efc305997d59b39
Andy Liang <andy.liang.1@stonybrook.edu>
2017-04-08 05:12:24 -0400

    Added Logo

---
+++ cordova/platforms/ios/www/js/initialize.js
@@ -0,0 +1,16 @@
+var app = new Framework7();
+var $$ = Dom7; //DOM Library
+
+var mainView = app.addView('.view-main', {
+    uniqueHistory: true,
+    domCache: true,
+    pushState: true,
+    material: true,
+    modalTitle: 'SmartStudy',
+    onAjaxStart: function (xhr) {
+        app.showIndicator();
+    },
+    onAjaxComplete: function (xhr) {
+        app.hideIndicator();
+    }
+});
