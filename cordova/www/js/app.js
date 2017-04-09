6f27ce7703e3a124f328a454a90e6b033a0477b1
Andy Liang <andy.liang.1@stonybrook.edu>
2017-04-08 20:46:39 -0400

    FINALLY WORKING

--- cordova/www/js/app.js
+++ cordova/www/js/app.js
@@ -28,15 +28,25 @@ app.onPageInit('results', function (page) {
 })
 
 function takePicture() {
-	navigator.camera.getPicture(onPictureSuccess, onPictureFail, { quality: 100, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.CAMERA });
-}
-
-function onPictureSuccess(imageURI) {
+	navigator.camera.getPicture(setOCRPicture, onPictureFail, { quality: 100, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.CAMERA });
 	$$('body').animate({'opacity': 1},{duration: 700,easing: 'linear'});
 	$$('#glasses').animate({'opacity': 0},{duration: 0,easing: 'linear'});
+}
+
+function takePictureEx() {
+	$$('body').animate({'opacity': 1},{duration: 700,easing: 'linear'});
+	$$('#glasses').animate({'opacity': 0},{duration: 0,easing: 'linear'});
+	//setOCRPicture('img/example.png');
+	$$('#image').attr('src', 'img/example.png');
+	
+}
+
+function imgLoaded() {
 	app.showIndicator();
-	getTextFromImage(imageURI, function (txtFromImage) {
-		summarizeText(txtFromImage, function (sumText) {
+	readText(document.getElementById('image'), function (res) {
+		$$('body').animate({'opacity': 1},{duration: 700,easing: 'linear'});
+		$$('#glasses').animate({'opacity': 0},{duration: 0,easing: 'linear'});
+		summarizeText(res, function (sumText) {
 			restext = sumText;
 			app.hideIndicator();
 			mainView.router.loadPage('pages/results.html');
@@ -44,7 +54,23 @@ function onPictureSuccess(imageURI) {
 	});
 }
 
+function setOCRPicture(imdata) {
+	$$('#image').attr('src', imdata);
+}
+
+function readText(image, callback) {
+	var canvas = document.getElementById("canvas");
+	canvas.width = image.naturalWidth;
+	canvas.height = image.naturalHeight;
+	var context = canvas.getContext("2d");
+	context.drawImage(image, 0, 0);
+
+	callback(OCRAD(context));
+
+};
+
 function onPictureFail(message) {
+	
 	app.alert('There was an error attempting to scan the document.', 'Error');
 }
 
@@ -62,7 +88,7 @@ $$('#pictureButton').on('click', function (e) {
 
 	}
 	)
-	/*setInterval(function(){
+	setTimeout(function(){
 		$$('body').animate(
 		{
 			'opacity': 0
@@ -72,8 +98,12 @@ $$('#pictureButton').on('click', function (e) {
 			easing: 'linear'
 		}
 		)
-	}, 1000);*/
-	setInterval(takePicture(), 1250);
+	}, 1000);
+	setTimeout(function(){
+		$$('body').animate({'opacity': 1},{duration: 1000, easing: 'linear'});
+		$$('#glasses').animate({'opacity': 1},{duration: 1000, easing: 'linear'});
+	}, 3000);
+	setTimeout(takePictureEx(), 3000);
 
 	
 });
@@ -85,17 +115,18 @@ function translateText(text, langTo, callback) {
 	});
 }
 
-function getTextFromImage(imageURI, callback) {
-	Tesseract.recognize(myImage).then(function(result){callback(result.text)});
+function getTextFromImage(myImage, callback) {
+	var strres = OCRAD(myImage);
+	callback(strres);
 }
 
-function summarizeText(text, callback) {
+function summarizeText(stext, callback) {
 	var summarizer = new JsSummarize();
-	var summary = summarizer.summarize(text);
-	var sr = ""
-	summary.forEach(function(sentence)
+	var summary = summarizer.summarize('', stext);
+	var sr = "";
+		summary.forEach(function(sentence)
 	{
-		sr.append(sentence+" ");
+		sr += sentence + " ";
 	});
 	callback(sr);
 }
